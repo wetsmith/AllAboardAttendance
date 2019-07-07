c4518896ecb37e9a774df981a906f5f0f8bb43ec
mtjacobsUCSC <52363843+mtjacobsUCSC@users.noreply.github.com>
2019-07-07 15:40:27 -0700

    Revert "Integrations sprint 1"

--- AllAboardAttendance/session/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import Session, Attendant, DirEdge
-
-admin.site.register(Session)
-admin.site.register(Attendant)
-admin.site.register(DirEdge)
-# Register your models here.
