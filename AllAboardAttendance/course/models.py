96207b3d9bdd6d37ac71eb189fbaea054b43fc7c
Wesley Smith <wetsmith@ucsc.edu>
2019-07-08 15:12:21 -0700

    added AllAboard project
    new app and lecture fixes

---
+++ AllAboardAttendance/course/models.py
@@ -0,0 +1,23 @@
+import datetime
+
+from django.db import models
+from django.utils import timezone
+
+from .Lecture.models import Lecture
+
+
+class Course(models.Model):
+    pub_date = models.DateTimeField('date published', default = timezone.now())
+    course_title = models.CharField(max_length=50)
+    #NEED TO ADD PROPER IMPORTS :D
+    lecture = ForeignKey(Lecture, on_delete=models.CASCADE)
+    student_ids = ForeignKey(Student, on_delete=models.CASCADE)
+    def __str__(self):
+        return self.course_title + ": " + str(self.pub_date.year)
+
+       
+class Student(models.Model):
+    student_id = models.CharField(max_length=20)
+    def __str__(self):
+        return self.student_id
+
