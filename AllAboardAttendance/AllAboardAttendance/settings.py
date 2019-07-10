f48f83c60dd38d583db751894127eaa325c75a49
Matthew Jacobs <mtjacobs@ucsc.edu>
2019-07-09 19:56:32 -0700

    Course
    Course model finished.
    
    In the view for course app you can add lectures with the students in a course to that lecture.

--- AllAboardAttendance/AllAboardAttendance/settings.py
+++ AllAboardAttendance/AllAboardAttendance/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'lecture.apps.LectureConfig',
+	'course.apps.CourseConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -106,7 +107,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'America/Los_Angeles'
 
 USE_I18N = True
 
