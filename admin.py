9dc6f9a110fd3fac043b407d642eee0242481ab4
Teweger <52230425+Teweger@users.noreply.github.com>
2019-07-12 17:43:26 -0700

    Add files via upload

---
+++ admin.py
@@ -0,0 +1,15 @@
+from django.contrib import admin
+from django.contrib.auth import get_user_model
+from django.contrib.auth.admin import UserAdmin
+
+from .forms import CreationForm, ChangeForm
+from .models import Teacher
+
+#updates our changes for admin to use
+class Admin(UserAdmin):
+    add_form = CreationForm
+    form = ChangeForm
+    model = Teacher
+    list_display = ['email', 'username',]
+
+admin.site.register(Teacher, Admin)
---
+++ views.py
@@ -0,0 +1,46 @@
+from django.shortcuts import render
+from django.shortcuts import redirect
+from django.contrib.auth.models import User
+
+from .models import Teacher
+from django.contrib import auth
+
+'''
+I added signup, login, logout functionality. 
+It basically uses Django's built in tools about authentication. 
+
+If you look at accounts/templages folder, then there's some html files about below view's. 
+'''
+
+def signup(request):
+    if request.method == "POST":
+        if request.POST["password1"]==request.POST["password2"]:
+            Teacher = User.objects.create_user(
+                username=request.POST["username"], password=request.POST["password1"]
+            )
+            auth.login(request, Teacher)
+            return redirect('home')
+        return render(request, 'signup.html')
+    return render(request, 'signup.html')
+
+def login(request):
+    if request.method == "POST":
+        username = request.POST["username"]
+        password = request.POST["password"]
+        Teacher = auth.authenticate(request, username=username, password=password)
+        if Teacher is not None:
+            auth.login(request, Teacher)
+            return redirect('home')
+        return render(request, 'login.html', {'error': 'username or password incorrect'})
+    return render(request, 'login.html')
+
+#For logout, I don't know about the mehtod post, so I just let it log out whenever we call that request.
+#it didn't worked when below lines were added.
+def logout(request):
+    #if request.method == "POST":
+    auth.logout(request)
+    return redirect('home')
+    #return render(request, 'login.html')
+
+def home(request):
+    return render(request, 'home.html')
