b47bce0c3fd867e4a55ce4c07a179f10d897d6c3
Matthew Jacobs <mtjacobs@ucsc.edu>
2019-07-15 21:02:53 -0700

    Task Nine Complete
    The page the link goes to still doesn't work but technically that is the next task.

--- AllAboardAttendance/course/urls.py
+++ AllAboardAttendance/course/urls.py
@@ -9,4 +9,5 @@ urlpatterns = [
     path('<slug:course_title_slug>/', views.DetailView.as_view(), name='detail'),
 	path('<int:course_id>/open-lecture/', views.open_lecture, name='open_lecture'),
 	path('<slug:lecture_title_slug>/info/', views.InfoView.as_view(), name='info'),
+	path('<slug:lecture_key_slug>/sign-in', views.SignInView.as_view(), name='sign_in')
 ]
