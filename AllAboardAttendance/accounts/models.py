670d03527719077ef89c1d90155d46e05c0a48bd
Teweger <52230425+Teweger@users.noreply.github.com>
2019-07-17 18:58:40 -0700

    added course fields to admin user creation

--- AllAboardAttendance/accounts/models.py
+++ AllAboardAttendance/accounts/models.py
@@ -3,25 +3,10 @@ from django.db import models
 from django.dispatch import dispatcher
 
 from django.contrib.auth.models import AbstractUser #basic django user model
+from course.models import Course
 
 class Teacher(AbstractUser):# overwrites the basic user model. 
-    pass
-    
+    course = models.ForeignKey(Course, on_delete=models.CASCADE, null = True)
+    #we can add any extra feilds here
     def __str__(self):
         return self.email
-    #we can add any extra feilds here
-
-#still working on this but should eventually makes creat user work for our Teacher usesr
-'''
-class UserManager(BaseUserManager):
-    def create_user(self, email, password=None):
-        if not email:
-            raise ValueError('Users must have an email address')
-        user = self.model(
-            email=self.normalize_email(email),
-        )
-
-        user.set_password(password)
-        user.save(using=self._db)
-        return user
-'''
