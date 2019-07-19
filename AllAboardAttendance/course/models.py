2f0a4c62cf91c4e1fd1d089b84080af3e0f9b6df
Teweger <52230425+Teweger@users.noreply.github.com>
2019-07-19 15:07:46 -0700

    added CourseList

--- AllAboardAttendance/course/models.py
+++ AllAboardAttendance/course/models.py
@@ -5,10 +5,11 @@ from django.db import models
 from django.utils import timezone
 from django.utils.text import slugify
 #from lecture import models as Lecture
-
+from accounts.models import CourseList
 
 class Course(models.Model):
 	# note: lecture model has a foriegn key to here!
+	course_list = models.ForeignKey(CourseList, on_delete=models.CASCADE, null = True)
 	pub_date = models.DateTimeField('date published', default = timezone.now())
 	course_title = models.CharField(max_length=50)
 	course_title_slug = models.SlugField(
@@ -21,9 +22,9 @@ class Course(models.Model):
 	# save also has broken link countermeasures.
 	def save(self, *args, **kwargs):
 		if not self.id:
-		# Newly created object, so set slug.
+			# Newly created object, so set slug.
 			self.course_title_slug = slugify(self.course_title)
-		super(Course, self).save(*args, **kwargs)
+			super(Course, self).save(*args, **kwargs)
 
 	def __str__(self):
 		return self.course_title + ": " + str(self.pub_date.year)
