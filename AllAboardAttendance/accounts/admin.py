a0aa227b39658bd6e79ea25ca939438d267c49a4
mtjacobsUCSC <52363843+mtjacobsUCSC@users.noreply.github.com>
2019-07-20 17:08:28 -0700

    removed username for email

--- AllAboardAttendance/accounts/admin.py
+++ AllAboardAttendance/accounts/admin.py
@@ -1,11 +1,27 @@
 from django.contrib import admin
-from django.contrib.auth import get_user_model
-from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.admin import UserAdmin as DjangoUserAdmin
+from django.utils.translation import ugettext_lazy as _
 
-from .forms import CreationForm, ChangeForm
 from .models import Teacher
 
-#updates our changes for admin to use
-class CustAdmin(UserAdmin):
-    pass
-admin.site.register(Teacher, CustAdmin)
+
+@admin.register(Teacher)
+class UserAdmin(DjangoUserAdmin):
+    """Define admin model for custom User model with no email field."""
+
+    fieldsets = (
+        (None, {'fields': ('email', 'password')}),
+        (_('Personal info'), {'fields': ('first_name', 'last_name')}),
+        (_('Permissions'), {'fields': ('is_active', 'is_staff', 'is_superuser',
+                                       'groups', 'user_permissions')}),
+        (_('Important dates'), {'fields': ('last_login', 'date_joined')}),
+    )
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'password1', 'password2'),
+        }),
+    )
+    list_display = ('email', 'first_name', 'last_name', 'is_staff')
+    search_fields = ('email', 'first_name', 'last_name')
+    ordering = ('email',)
