40cbb2e546754a3d529977b5892a07b8ca78430e
Teweger <52230425+Teweger@users.noreply.github.com>
2019-07-21 11:49:07 -0700

    finished up admin page

--- AllAboardAttendance/accounts/models.py
+++ AllAboardAttendance/accounts/models.py
@@ -1,12 +1,9 @@
-from django.contrib.auth.models import User
 from django.db import models
+from django.contrib.auth.models import User
 from django.dispatch import dispatcher
-
 from django.contrib.auth.models import AbstractUser, BaseUserManager #basic django user model
-#from course.models import Course
 
-class UserManager(BaseUserManager):
-    """Teacher Model Manager"""
+class UserManager(BaseUserManager):#manages the creation and set up of users both Teacher and Superuser
     use_in_migrations=True
     
     def _create_user(self, email, password, **extra_fields):#general user creation
@@ -18,31 +15,25 @@ class UserManager(BaseUserManager):
         teacher.save(using=self._db)
         return teacher
 
-    def create_user(self, email, password=None, **extra_fields):
-        """Create and save a regular User with the given email and password."""
+    def create_user(self, email, password=None, **extra_fields):#creates a teacher
         extra_fields.setdefault('is_staff', False)
         extra_fields.setdefault('is_superuser', False)
         return self._create_user(email, password, **extra_fields)
 
-    def create_superuser(self, email, password, **extra_fields):
-        """Create and save a SuperUser with the given email and password."""
+    def create_superuser(self, email, password, **extra_fields):#creates a superuser
         extra_fields.setdefault('is_staff', True)
         extra_fields.setdefault('is_superuser', True)
-
         if extra_fields.get('is_staff') is not True:
             raise ValueError('Superuser must have is_staff=True.')
         if extra_fields.get('is_superuser') is not True:
             raise ValueError('Superuser must have is_superuser=True.')
-
         return self._create_user(email, password, **extra_fields)    
     
     
 class Teacher(AbstractUser):# overwrites the basic user model. 
-    """Teacher model"""
-    
-    username =None
+    username =None#email will be used as our username
     email = models.EmailField( ('email address'), unique=True)
     
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = []
-    objects = UserManager()
+    objects = UserManager()#sets the magager for teacher as our UserManager
